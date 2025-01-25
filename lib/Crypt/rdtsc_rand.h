22fc536d5552a8dd4f363f3f2ac6937d1fb0b41e
Scott Baker <scott@perturb.org>
2025-01-25 10:29:17 -0800

    Upstream rdtsc_rand fixes

--- lib/Crypt/rdtsc_rand.h
+++ lib/Crypt/rdtsc_rand.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <time.h> // for clock_gettime()
 
 ////////////////////////////////////////////////////////////////////////////////
 // rdtsc_rand.h: v0.5pre
@@ -7,7 +8,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 // Multiply-Shift Hash (Passes SmallCrush and PractRand up to 128GB)
-uint64_t hash_msh(uint64_t x) {
+static uint64_t hash_msh(uint64_t x) {
 	uint64_t prime = 0x9e3779b97f4a7c15; // A large prime constant
 	x ^= (x >> 30);
 	x *= prime;
@@ -18,7 +19,7 @@ uint64_t hash_msh(uint64_t x) {
 }
 
 // MurmurHash3 Finalizer (Passes SmallCrush and PractRand up to 32GB)
-uint64_t hash_mur3(uint64_t x) {
+static uint64_t hash_mur3(uint64_t x) {
 	x ^= x >> 33;
 	x *= 0xff51afd7ed558ccd;
 	x ^= x >> 33;
@@ -26,6 +27,27 @@ uint64_t hash_mur3(uint64_t x) {
 	x ^= x >> 33;
 	return x;
 }
+
+#if (defined(__ARM_ARCH))
+// Nanoseconds since Unix epoch
+static uint64_t rdtsc_nanos() {
+	struct timespec ts;
+
+	// int8_t ok = clock_gettime(CLOCK_MONOTONIC, &ts); // Uptime
+	int8_t ok = clock_gettime(CLOCK_REALTIME, &ts);     // Since epoch
+
+	if (ok != 0) {
+		return 0; // Return 0 on failure (you can handle this differently)
+	}
+
+	// Calculate nanoseconds
+	uint64_t ret = (uint64_t)ts.tv_sec * 1000000000ULL + (uint64_t)ts.tv_nsec;
+
+	//printf("N: %llu\n", ret);
+
+	return ret;
+}
+#endif
 
 //////////////////////////////////////
 // End hashing function definitions //
@@ -37,7 +59,7 @@ uint64_t hash_mur3(uint64_t x) {
 #endif
 
 // Get the instruction counter for various CPU/Platforms
-uint64_t get_rdtsc() {
+static uint64_t get_rdtsc() {
 #if defined(_WIN32) || defined(_WIN64)
 	return __rdtsc();
 #elif defined(__aarch64__) || defined(__arm64)
@@ -50,6 +72,8 @@ uint64_t get_rdtsc() {
 	uint32_t low, high;
 	__asm__ volatile ("rdtsc" : "=a"(low), "=d"(high));
 	return ((uint64_t)(high) << 32) | low;
+#elif (defined(__ARM_ARCH))
+	return rdtsc_nanos();
 #else
 	#warning "rdtsc_rand: Unknown system type. Results will be 0."
 	return 0;
@@ -57,7 +81,7 @@ uint64_t get_rdtsc() {
 }
 
 // Get an unsigned 64bit random integer
-uint64_t rdtsc_rand64() {
+static uint64_t rdtsc_rand64() {
 	// Hash the rdtsc value through hash64
 	uint64_t rdtsc_val = get_rdtsc();
 	uint64_t ret       = hash_msh(rdtsc_val);
